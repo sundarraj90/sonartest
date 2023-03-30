381daa044e6793d0b113bf9a89c0bc4e546c4ee0
sundarraj90 <sundarraj.g@techbumbles.com>
2023-03-30 05:46:13 +0000

    code changed

--- sonar.java
+++ sonar.java
@@ -1,6 +1,7 @@
 #This is the first program
 import java.io
-public class HelloWorld {
+public class HelloWorld (
+		{
     public static void main(String[] args) {
         System.out.println("Hello, world!");
     }
